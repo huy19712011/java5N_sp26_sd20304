848e519e5444937501d8d4734210f24b15fc16e8
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-01-05 08:50:22 +0700

    set up spring boot MVC project

---
+++ src/main/java/org/example/java5n_sp26_sd20304/Java5NSp26Sd20304Application.java
@@ -0,0 +1,14 @@
+package org.example.java5n_sp26_sd20304;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5NSp26Sd20304Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java5NSp26Sd20304Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/java5n_sp26_sd20304/Java5NSp26Sd20304ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java5n_sp26_sd20304;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java5NSp26Sd20304ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
